1a6f6b12f06ad03e3027efa685c5829db59e2f61
n2pro <prohorov.nn@gmail.com>
2018-06-15 16:16:52 +0200

    Added calc

---
+++ GitStart/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitStart
+{
+    public class Calculator
+    {
+        public int Sum( int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Dif(int a, int b)
+        {
+            return a - b;
+        }
+
+    }
+}
