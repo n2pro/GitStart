31d84ecf844338cb89c79f3c7a5d69b3357f4c01
n2pro <prohorov.nn@gmail.com>
2018-06-15 13:32:09 +0200

    Add project files.

---
+++ GitStart/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitStart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
